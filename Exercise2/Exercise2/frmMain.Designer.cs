5770134f8f89e712c6a9665f0f8092a6cba97095
md-bd <mohammad.buet@gmail.com>
2020-03-11 12:44:44 +0600

    Add Printing Options

---
+++ Exercise2/Exercise2/frmMain.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Exercise2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.cmdPrint = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmdPrint
+            // 
+            this.cmdPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdPrint.Location = new System.Drawing.Point(638, 13);
+            this.cmdPrint.Name = "cmdPrint";
+            this.cmdPrint.Size = new System.Drawing.Size(124, 62);
+            this.cmdPrint.TabIndex = 0;
+            this.cmdPrint.Text = "Print";
+            this.cmdPrint.UseVisualStyleBackColor = true;
+            this.cmdPrint.Click += new System.EventHandler(this.cmdPrint_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(6, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(301, 30);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "This text box is on top left corner";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(454, 414);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(342, 30);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "This text box is on bottom right corner.";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cmdPrint);
+            this.Name = "frmMain";
+            this.Text = "Print Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button cmdPrint;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Exercise2/Exercise2/frmMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Printing;
+
+
+namespace Exercise2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdPrint_Click(object sender, EventArgs e)
+        {
+            cmdPrint.Visible = false;
+
+
+            PrintPreviewDialog ppd = new PrintPreviewDialog();
+
+            ppd.Document = new PrintDocument();
+            ppd.Document.PrintPage += new PrintPageEventHandler(Document_PrintPage);
+
+            ppd.ShowDialog();
+            cmdPrint.Visible = true;
+        }
+
+        void Document_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Bitmap bt = new Bitmap(this.Width, this.Height);
+
+            /*
+            // Determine Scaling
+            float scale = 1.0f;
+            scale = Math.Min(scale, (float)e.MarginBounds.Width / bt.Width);
+            scale = Math.Min(scale, (float)e.MarginBounds.Height / bt.Height);
+
+            e.Graphics.TranslateTransform(e.MarginBounds.Left +
+                (e.MarginBounds.Width - bt.Width * scale) / 2, e.MarginBounds.Top);
+            e.Graphics.ScaleTransform(scale, scale);
+            //------------------------------------------------
+            */
+
+            DrawToBitmap(bt, new Rectangle(0, 0, this.Width, this.Height));
+            RectangleF destinationRect = new RectangleF(0, 0, this.Width, this.Height);
+
+            //Draw a portion of the image. Scale that portion of the image
+            RectangleF sourceRect = new RectangleF(0, 0, this.Width, this.Height);
+            e.Graphics.DrawImage(bt, destinationRect, sourceRect, GraphicsUnit.Pixel);
+
+        }
+    }
+}
